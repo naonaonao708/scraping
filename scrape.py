01f0d3cdb0d2e197287e0ce5389c2a2b12f7347a
Naoya <86583481+naonaonao708@users.noreply.github.com>
2021-11-05 22:02:56 +0900

    update

--- scrape.py
+++ scrape.py
@@ -1,10 +1,19 @@
 import csv
 import requests
-import bs4
+from bs4 import BeautifulSoup
 import pandas as pd
+import time
+from datetime import datetime as dt
+import re
+from collections import Counter
+
 
 #菊花賞のデータをとってくる
 race_id = "202109040611"
 url_base = "https://db.netkeiba.com/race/"
 
 url = url_base + race_id
+race_html = requests.get(url)
+race_html.encoding = race_html.apparent_encoding
+race_soup = BeautifulSoup(race_html.text, 'html.parser')
+print(url)
